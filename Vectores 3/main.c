e6c4ab36df38e01974eac644f68b4c297ae70ba9
jmgl0031 <jmgl0031@red.ujaen.es>
2026-02-24 11:42:19 +0100

    Actualización Vectores 3 para hacer ejercicio

--- Vectores 3/main.c
+++ Vectores 3/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-
+#include "stats.h"
 int main(void) {
-    printf("Hello, World!\n");
+    int a[] = {3, -1, 8, 8, 2};
+    size_t n = sizeof(a)/sizeof(a[0]);
+
+    printf("max=%d\n", max_int(a, n));
+    printf("min=%d\n", min_int(a, n));
+    printf("media=%.2f\n", media_int(a, n));
+
+    double b[] = {1.0, 2.0, 3.0, 4.0};
+    size_t m = sizeof(b)/sizeof(b[0]);
+
+    normaliza(b, m);
+    printf("normalizado: ");
+    for (size_t i = 0; i < m; ++i) printf("%.3f ", b[i]);
+    printf("\n");
+
     return 0;
 }
---
+++ Vectores 3/stats.c
@@ -0,0 +1,14 @@
+#include "stats.h"
+#include <math.h>
+int max_int(const int *v, size_t n) {
+    // TODO
+}
+int min_int(const int *v, size_t n) {
+    // TODO
+}
+double media_int(const int *v, size_t n) {
+    // TODO
+}
+void normaliza(double *v, size_t n) {
+    // TODO
+}
