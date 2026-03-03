0002f1095a0f876d6b8d48b7bf6392cd6285aaff
jmgl0031 <jmgl0031@red.ujaen.es>
2026-03-03 14:59:21 +0100

    Comienzo de implemetacion en main

--- Propuesta practica/main.c
+++ Propuesta practica/main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
 #include "funciones.h"
 
 int main(void) {
     int n;
-    leer_entero("Edad: ", &n);
-    printf("El valor es: %d",n);
+    int edad;
+    float altura;
+    int nombre;
+
+    printf("========== MENU ==========\n");
+    printf("1: Introducir edad.\n");
+    printf("2: Introducir altura (m).\n");
+    printf("3: Introducir nombre.\n");
+
+    leer_entero("Elija una opción:  ", &n);
+        while (n > 3 && n < 1) {
+            printf("Opción inválida\n");
+            printf("========== MENU ==========\n");
+            printf("1: Introducir edad.\n");
+            printf("2: Introducir altura (m).\n");
+            printf("3: Introducir nombre.\n");
+            leer_entero("Elija una opción:  ", &n);
+        }
+    switch (n) {
+        case 1: leer_entero("Introduzca su edad:  ", &edad);
+            printf("Su edad es: %d.\n", edad);
+
+        case 2: leer_float("Introduzca su altura (m):  ", &altura);
+            printf("Su altura es: %f.\n", edad);
+
+        //case 3: leer_cadena("Introuzca su nombre: \n", &nombre);//
+    }
+
 }
